e3a74ea22263971c98f62a949c9dba465ed1e634
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-04-19 20:19:09 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "27498866"
 api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
-bot_token = "7055374830:AAGwywLzX86tLB6V4BMPgpTv7h0iFYrEw54"
+bot_token = "7125899687:AAF8ZHqtziwW20yENI0J8kfxIcfatybfsf0"
 
